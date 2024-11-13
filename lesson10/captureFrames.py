ba0bf7d35be2a41cf7991302f540bb2d833d18d5
Andrii Kolomiiets <andrii.kolomiets@vgentertainment.com>
2024-11-13 14:48:41 +0200

    + hw10

--- lesson10/captureFrames.py
+++ lesson10/captureFrames.py
@@ -3,13 +3,15 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import time
+import os
 
-stream = cv2.VideoCapture("17000628-hd_1920_1080_60fps.mp4")
+stream = cv2.VideoCapture("vtest.avi")
 
 ret, frame = stream.read()
 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-tgtRect = [1050,70,1160,320]
+tgtRect = [10,70,100,200]
+#tgtRect = [1050,70,1160,320]
 tgtBbox = [tgtRect[0], tgtRect[1], tgtRect[2]-tgtRect[0], tgtRect[3]-tgtRect[1]]
 
 ### Step 2
@@ -18,19 +20,17 @@ trackerKCF.init(frame, tgtBbox)
 trackerCSRT = cv2.TrackerCSRT_create()
 trackerCSRT.init(frame, tgtBbox)
 
-#draw initial frame
 cv2.rectangle(frame, tgtRect[:2], tgtRect[2:], (0, 255, 0), 3)
 plt.imshow(frame)
 plt.show(), plt.draw()    
-plt.waitforbuttonpress(0.1)
-plt.clf()
+#plt.waitforbuttonpress(0.1)
+#plt.clf()
 
-
-#process all video
 def drawBox(img, bbox, clr):
     cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), clr, 2)
-    
-while False:
+
+ii=0
+while True:
 #for i in range(2):
     ret, frame = stream.read()
     
@@ -40,16 +40,21 @@ while False:
     
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
-    kcfResult, kcfBbox = trackerKCF.update(frame)
-    csrtResult, csrtBbox = trackerCSRT.update(frame)
-    if kcfResult:
-        drawBox(frame, kcfBbox, (0, 255, 0))
+#    kcfResult, kcfBbox = trackerKCF.update(frame)
+#    csrtResult, csrtBbox = trackerCSRT.update(frame)
+#    if kcfResult:
+#        drawBox(frame, kcfBbox, (0, 255, 0))
 
-    if csrtResult:
-        drawBox(frame, csrtBbox, (255, 0, 0))
+#    if csrtResult:
+#        drawBox(frame, csrtBbox, (255, 0, 0))
         
-    plt.imshow(frame)
-    plt.show(), plt.draw()    
-    plt.waitforbuttonpress(0.1)
-    plt.clf()
-    time.sleep(0.1)
+#    plt.imshow(frame)
+#    plt.show(), plt.draw()    
+#    plt.waitforbuttonpress(0.1)
+#    plt.clf()
+    
+#    time.sleep(0.1)
+    fname = 'frame_' + str(ii).zfill(3) + '.jpg'
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(os.path.join('c:/users/stoianov/cv-study/lesson10/data/', fname), frame)
+    ii = ii+1
--- lesson10/homework.py
+++ lesson10/homework.py
@@ -1,37 +1,47 @@
-### Step 1
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import time
 
-stream = cv2.VideoCapture("hd_1920_1080_60fps.mp4")
+### Step 1
+#Decide what video you are going to use for this homework, select an object and generate the template. You can use any video you want (your own, from Youtube, etc.)
+#and track any object you want (e.g. a car, a pedestrian, etc.).
 
-ret, frame = stream.read()
+stream = cv2.VideoCapture("istockphoto-1155019062-640_adpp_is.mp4")
+
+# target appears on 5th frame
+for _ in range(5):
+	ret, frame = stream.read()
+ 
 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-tgtRect = [1050,70,1160,320]
+tgtRect = [665,270,760,340]
 tgtBbox = [tgtRect[0], tgtRect[1], tgtRect[2]-tgtRect[0], tgtRect[3]-tgtRect[1]]
 
 ### Step 2
+#Initialize a tracker (e.g. KCF).
+
 trackerKCF = cv2.TrackerKCF_create()
 trackerKCF.init(frame, tgtBbox)
 trackerCSRT = cv2.TrackerCSRT_create()
 trackerCSRT.init(frame, tgtBbox)
 
-#draw initial frame
 cv2.rectangle(frame, tgtRect[:2], tgtRect[2:], (0, 255, 0), 3)
-plt.imshow(frame)
-plt.show(), plt.draw()    
-plt.waitforbuttonpress(0.1)
-plt.clf()
 
+if 1:
+	plt.imshow(frame)
+	plt.draw()    
+	plt.waitforbuttonpress(1.5)
 
-#process all video
 def drawBox(img, bbox, clr):
     cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), clr, 2)
-    
-while False:
-#for i in range(2):
+
+### Step 3
+#Run the tracker on the video and the selected object. Run the tracker for around 10-15 frames.
+kcfTime = 0
+csrtTime = 0
+
+while True:
     ret, frame = stream.read()
     
     if not ret:
@@ -40,8 +50,14 @@ while False:
     
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
+    startTime = time.perf_counter()
     kcfResult, kcfBbox = trackerKCF.update(frame)
+    kcfTime = kcfTime + time.perf_counter()-startTime
+
+    startTime = time.perf_counter()
     csrtResult, csrtBbox = trackerCSRT.update(frame)
+    csrtTime = csrtTime + time.perf_counter()-startTime
+         
     if kcfResult:
         drawBox(frame, kcfBbox, (0, 255, 0))
 
@@ -49,7 +65,12 @@ while False:
         drawBox(frame, csrtBbox, (255, 0, 0))
         
     plt.imshow(frame)
-    plt.show(), plt.draw()    
-    plt.waitforbuttonpress(0.1)
+    plt.draw()    
+    plt.waitforbuttonpress(0.01)
     plt.clf()
-    time.sleep(0.1)
+
+print("KCF time:", kcfTime)
+print("CSRT time:", csrtTime)
+
+# по результатам может уверенно сказать, что CSRT (красный бокс) уверенно лучше справляется 
+# с трекингом обьектов которые меняют свои размеры, при этом более ресурсоемкий, в моем случае в 8 раз (0.53с против 4.1с)
